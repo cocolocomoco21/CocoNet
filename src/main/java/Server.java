9843b15d86d26887e563cbc0dc499475b8b1c523
Matt Henricks <mchenricks@wisc.edu>
2018-08-06 22:11:29 -0700

    Skeleton for registerPeer function, make oo not static

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,22 +1,46 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import spark.Request;
+import spark.Response;
 import spark.RouteGroup;
 
-import static spark.Spark.init;
+import static spark.Spark.before;
+import static spark.Spark.get;
 import static spark.Spark.path;
-import static spark.Spark.*;
-import static spark.Spark.staticFiles;
+import static spark.Spark.post;
+
 
 public class Server {
+    
+    static final String CONTENT_TYPE_JSON = "application/json";
+    static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private Server server;
+
+
+    private Server() {
+        // TODO
+    }
+
     public static void main(String[] args) {
         System.out.println("Starting server");
 
-        path("/p2p", routes());
+        Server server = new Server();
+        path("/server", server.routes());
+
+        System.out.println("Server running...");
     }
 
-    public static RouteGroup routes() {
+    private RouteGroup routes() {
         return () ->  {
             before("/*", (request, response) -> System.out.println("endpoint: " + request.pathInfo()));
-            post("/register", (request, response) -> "POST to register");
+            post("/register", this::registerPeer, gson::toJson);
             get("/", (request, response) -> "GET to fetch");
         };
     }
+
+    private boolean registerPeer(Request request, Response response) {
+        response.type(CONTENT_TYPE_JSON);
+        return true;
+    }
+
 }
