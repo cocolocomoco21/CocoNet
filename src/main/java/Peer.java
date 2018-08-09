f285629c6478a308c281e31fdf7652c94f59845f
Matt Henricks <mchenricks@wisc.edu>
2018-08-08 18:26:02 -0700

    Add main in Peer to be able to run alongside Server. Start implementing Peer

--- src/main/java/Peer.java
+++ src/main/java/Peer.java
@@ -1,12 +1,36 @@
+import java.net.InetAddress;
+
+
 public class Peer {
     
     String ipAddress;
     String friendlyName;
 
+    public static void main(String[] args) {
+        Peer peer = new Peer("Matt's Lenovo");
+    }
     
+    private Peer(String friendlyName) {
+        initialize(friendlyName);
+    }
+
     public Peer(Registration registration) {
         this.ipAddress = registration.getIPAddress();
         this.friendlyName = registration.getFriendlyName();
+    }
+
+    private void initialize(String friendlyName) {
+        this.friendlyName = friendlyName;
+
+        try {
+            this.ipAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            // This should probably be safe and return failure, but for now just swallow the bad 
+            // IP and fail silently. Validation is required later on
+            // TODO
+            //return false;
+            System.out.println("Invalid IP");
+        }
     }
 
     @Override
